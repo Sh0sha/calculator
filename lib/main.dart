81f237b2884af08ec6516d7a8fabcea7abc02465
Шохзод Сайдирахманович Шаимкулов <siberf@mail.ru>
2024-03-19 15:23:39 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,290 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String number1 = "";
+  String operand = "";
+  String number2 = "";
+
+  // переопределяем
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                reverse: true,
+                child: Container(
+                  alignment: Alignment.bottomLeft,
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    "$number1$operand$number2".isEmpty
+                        ? "0"
+                        : "$number1$operand$number2",
+                    style: const TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+              ),
+            ),
+            Wrap(   // настройка н0 и размера, расположении кнопок
+              children: Btn.buttonValues
+                  .map(
+                    (value) => SizedBox(
+                  width: value == Btn.n0
+                      ? screenSize.width / 2
+                      : (screenSize.width / 4),
+                  height: screenSize.width / 4,
+                  child: buildButton(value),
+                ),
+              )
+                  .toList(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildButton(value) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Material(
+        color: getBtnColor(value),
+        clipBehavior: Clip.hardEdge,
+        shape: OutlineInputBorder(
+          borderSide: const BorderSide(
+            color: Colors.white38,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: InkWell(
+          onTap: () => onBtnTap(value),
+          child: Center(
+            child: Text(
+              value,
+              style: const TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onBtnTap(String value) {
+    if (value == Btn.del) {
+      delete();
+      return;
+    }
+
+    if (value == Btn.clr) {
+      clearAll();
+      return;
+    }
+
+    if (value == Btn.power) {
+      setOperand(value);
+      return;
+    }
+
+    if (value == Btn.calculate) {
+      calculate();
+      return;
+    }
+
+    appendValue(value);
+  }
+
+  void setOperand(String value) {
+    if (number1.isNotEmpty && operand.isEmpty && number2.isEmpty) {
+      operand = value;
+      setState(() {});
+    }
+  }
+
+  void calculate() {
+    if (number1.isEmpty) return;
+    if (operand.isEmpty) return;
+    if (number2.isEmpty) return;
+
+    final double num1 = double.parse(number1);
+    final double num2 = double.parse(number2);
+
+    var result = 0.0;
+    switch (operand) {
+      case Btn.add:
+        result = num1 + num2;
+        break;
+      case Btn.subtract:
+        result = num1 - num2;
+        break;
+      case Btn.multiply:
+        result = num1 * num2;
+        break;
+      case Btn.divide:
+        result = num1 / num2;
+        break;
+      case Btn.power:
+        result = customPow(num1, num2);
+        break;
+      default:
+    }
+
+    setState(() {
+      number1 = result.toStringAsPrecision(6);
+
+      if (number1.endsWith(".0")) {
+        number1 = number1.substring(0, number1.length - 2);
+      }
+
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  double customPow(double base, double exponent) {
+    double result = 1.0;
+    for (int i = 0; i < exponent.abs(); i++) {
+      result *= base;
+    }
+    return exponent < 0 ? 1 / result : result;
+  }
+
+  void clearAll() {
+    setState(() {
+      number1 = "";
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  void delete() {
+    if (number2.isNotEmpty) {
+      number2 = number2.substring(0, number2.length - 1);
+    } else if (operand.isNotEmpty) {
+      operand = "";
+    } else if (number1.isNotEmpty) {
+      number1 = number1.substring(0, number1.length - 1);
+    }
+
+    setState(() {});
+  }
+
+  void appendValue(String value) {
+    if (value != Btn.dot && int.tryParse(value) == null) {
+      if (operand.isNotEmpty && number2.isNotEmpty) {
+        calculate();
+      }
+      operand = value;
+    } else if (number1.isEmpty || operand.isEmpty) {
+      if (value == Btn.dot && number1.contains(Btn.dot)) return;
+      if (value == Btn.dot && (number1.isEmpty || number1 == Btn.n0)) {
+        value = "0.";
+      }
+      number1 += value;
+    } else if (number2.isEmpty || operand.isNotEmpty) {
+      if (value == Btn.dot && number2.contains(Btn.dot)) return;
+      if (value == Btn.dot && (number2.isEmpty || number2 == Btn.n0)) {
+        value = "0.";
+      }
+      number2 += value;
+    }
+
+    setState(() {});
+  }
+
+  Color getBtnColor(value) {
+    return [Btn.del, Btn.clr].contains(value)
+        ? Colors.orange
+        : [
+      Btn.power,
+      Btn.multiply,
+      Btn.add,
+      Btn.subtract,
+      Btn.divide,
+      Btn.calculate,
+    ].contains(value)
+        ? Colors.orange
+        : Colors.black;
+
+  }
+}
+
+class Btn {
+  static const String del = "<-";
+  static const String clr = "C";
+  static const String power = "^";
+  static const String multiply = "×";
+  static const String divide = "÷";
+  static const String add = "+";
+  static const String subtract = "-";
+  static const String calculate = "=";
+  static const String dot = ".";
+
+  static const String n0 = "0";
+  static const String n1 = "1";
+  static const String n2 = "2";
+  static const String n3 = "3";
+  static const String n4 = "4";
+  static const String n5 = "5";
+  static const String n6 = "6";
+  static const String n7 = "7";
+  static const String n8 = "8";
+  static const String n9 = "9";
+
+  static const List<String> buttonValues = [
+    power,
+    multiply,
+    del,
+    clr,
+    divide,
+    n1,
+    n2,
+    n3,
+    subtract,
+    n4,
+    n5,
+    n6,
+    add,
+    n7,
+    n8,
+    n9,
+    calculate,
+    dot,
+    n0,
+  ];
+}
